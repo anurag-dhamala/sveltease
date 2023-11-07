2888fd76608b7f10fed5ba1d2080b8946e13abaf
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-07 22:46:27 +0545

    feat: add cleanup method and update the props manually

--- src/SvelteComponent.ts
+++ src/SvelteComponent.ts
@@ -1,16 +1,17 @@
 import { ComponentType } from "svelte";
 import { NonExistentComponentError, NonExistentElementError, NonExistentIdError } from "./errors";
-import { SCOptions } from "./types";
+import { SveltifyComponentOpts } from "./types";
+import { SvelteComponentKV } from "./store";
 
 
 export class SvelteComponent {
 
     private Component: ComponentType<any> | null = null;
     private elementId: string = "";
-    private options: SCOptions | null = null;
+    private options: SveltifyComponentOpts | null = null;
 
 
-    constructor(Component: ComponentType<any>, elementId: string, options?: SCOptions) {
+    constructor(Component: ComponentType<any>, elementId: string, options?: SveltifyComponentOpts) {
         this.Component = Component;
         this.validateId(elementId);
         if(options) {
@@ -23,18 +24,47 @@ export class SvelteComponent {
         if(!this.Component) {
             throw new NonExistentComponentError();
         }
-        const component = new this.Component({
-            target: document.getElementById(self.elementId) as Element,
-            props: self.options?.props
-        })
-       
-        console.log("the component : ", component)
+        if(!this.checkIfAlreadyExists(self.elementId)) {
+            this.createNew();
+        } else {
+            this.update();
+        }
+        
         return self;
     }
 
     cleanup() {
         if(!this.Component) throw new NonExistentComponentError();
+        delete SvelteComponentKV[this.elementId];
+        //@ts-ignore
         this.Component.$destroy();
+    }
+
+    private checkIfAlreadyExists(id: string) {
+        if(!SvelteComponentKV[id]) return false;
+        return true;
+    }
+
+    private createNew() {
+        const self = this;
+        if(!self.Component) {
+            throw new NonExistentComponentError();
+        }
+        const component = new self.Component({
+            target: document.getElementById(self.elementId) as Element,
+            props: self.options?.props
+        })
+        self.Component = component;
+        SvelteComponentKV[self.elementId] = component;
+    }
+
+    private update() { 
+        const self = this;
+        const component = SvelteComponentKV[self.elementId];
+        //@ts-ignore
+        component.$set({
+            ...self.options?.props
+        })
     }
 
     private validateId(id: string) {
