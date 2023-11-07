be752fce533ce3b35095c6b4edba92499e035d4f
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-07 23:38:07 +0545

    feat: sveltify return type change to promise

--- src/sveltify.ts
+++ src/sveltify.ts
@@ -2,10 +2,15 @@ import { ComponentType } from "svelte";
 import { NonExistentComponentError, NonExistentIdError } from "./errors";
 import { SveltifyComponent } from "./SveltifyComponent";
 import { SveltifyComponentOpts } from "./types";
+import { renderAfter } from "./utils";
 
-export const sveltify =(Component: ComponentType<any>, id: string, options: SveltifyComponentOpts)=> {
-        if(!Component) throw new NonExistentComponentError()
-        if(!id) throw new NonExistentIdError();
+export const sveltify =(Component: ComponentType<any>, id: string, options: SveltifyComponentOpts): Promise<SveltifyComponent>=> {
+     return new Promise<SveltifyComponent>((resolve, reject) => {
+        renderAfter(()=> {
+        if(!Component) reject(new NonExistentComponentError());
+        if(!id) reject(new NonExistentIdError());
         const sveltifier = new SveltifyComponent(Component, id, options);
-        return sveltifier.ignite();
+        resolve(sveltifier.ignite());
+        })
+    })
 }
