5a66f8e939e3b316f9daf46cc11f29191b5b95d8
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-07 22:45:56 +0545

    feat: return sveltecomponent for cleanup purposes

--- src/sveltify.ts
+++ src/sveltify.ts
@@ -1,14 +1,11 @@
 import { ComponentType } from "svelte";
-import { renderAfter } from "./utils";
-import { ComponentNotFoundError, NonExistentIdError } from "./errors";
+import { NonExistentComponentError, NonExistentIdError } from "./errors";
 import { SvelteComponent } from "./SvelteComponent";
-import { SCOptions } from "./types";
+import { SveltifyComponentOpts } from "./types";
 
-export const sveltify =(Component: ComponentType<any>, id: string, options: SCOptions)=> {
-    renderAfter(()=> {
-        if(!Component) throw new ComponentNotFoundError()
+export const sveltify =(Component: ComponentType<any>, id: string, options: SveltifyComponentOpts)=> {
+        if(!Component) throw new NonExistentComponentError()
         if(!id) throw new NonExistentIdError();
         const sveltifier = new SvelteComponent(Component, id, options);
-        sveltifier.ignite();
-    })
+        return sveltifier.ignite();
 }
